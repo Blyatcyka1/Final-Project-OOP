c9d655b51bb68c24fd90f7e0f74acec6144dd4e6
Blyatcyka1 <145783613+Blyatcyka1@users.noreply.github.com>
2023-11-20 20:08:59 +0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main{
+	public static void main (String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("      HANGMAN");
+		
+		System.out.println("  ||1. Easy     ||");
+		System.out.println("  ||2. Hard     ||");
+		System.out.println("  ||3. Extreme  ||");
+		System.out.print("Please choose a dificulty:");
+		int difficultylevel = scanner.nextInt();
+		
+		switch(difficultylevel) {
+		case 1: 
+			//Enter easy method
+			System.out.println("You have chosen easy");
+			break;
+		case 2:
+			// Enter hard method 
+			System.out.println("You have chosen hard");
+			break;
+		case 3: 
+			//Enter extreme method 
+			System.out.println("You have chosen extreme");
+			break;
+		}
+		
+		
+	}
+}
+
+
+
